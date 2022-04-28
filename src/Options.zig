1914bff38c238e7468e65e9a7f76bd5704eb089c
Gregory Anders <greg@gpanders.com>
2022-04-28 16:45:23 -0600

    Update clap version

--- src/Options.zig
+++ src/Options.zig
@@ -1,31 +1,10 @@
 const std = @import("std");
 const clap = @import("clap");
+const stderr = std.io.getStdErr().writer();
 
 const Options = @This();
 
 const config = @cImport(@cInclude("config.h"));
-
-const usage_str =
-    \\Usage: fzy [OPTION]...
-    \\ -l, --lines LINES        Specify how many lines of results to show (default 10)
-    \\ -p, --prompt PROMPT      Input prompt (default '> ')
-    \\ -q, --query QUERY        Use QUERY as the initial search string
-    \\ -e, --show-matches QUERY Output the sorted matches of QUERY
-    \\ -t, --tty=TTY            Specify file to use as TTY device (default /dev/tty)
-    \\ -s, --show-scores        Show the scores of each match
-    \\ -0, --read-null          Read input delimited by ASCII NUL characters
-    \\ -j, --workers NUM        Use NUM workers for searching. (default is # of CPUs)
-    \\ -i, --show-info          Show selection info line
-    \\ -f, --file FILE          Read choices from FILE insted of stdin
-    \\ -n, --no-sort            Do not sort matches
-    \\ -h, --help               Display this help and exit
-    \\ -v, --version            Output version information and exit
-    \\
-;
-
-fn usage() void {
-    std.debug.print(usage_str, .{});
-}
 
 benchmark: u32 = 0,
 filter: ?[]const u8 = null,
@@ -44,36 +23,57 @@ sort: bool = true,
 pub fn new() !Options {
     var options = Options{};
 
-    const params = comptime [_]clap.Param(clap.Help){
-        clap.parseParam("-h, --help") catch unreachable,
-        clap.parseParam("-l, --lines <LINES>") catch unreachable,
-        clap.parseParam("-p, --prompt <PROMPT>") catch unreachable,
-        clap.parseParam("-q, --query <QUERY>") catch unreachable,
-        clap.parseParam("-e, --show-matches <QUERY>") catch unreachable,
-        clap.parseParam("-t, --tty <TTY>") catch unreachable,
-        clap.parseParam("-s, --show-scores") catch unreachable,
-        clap.parseParam("-0, --read-null") catch unreachable,
-        clap.parseParam("-j, --workers <NUM>") catch unreachable,
-        clap.parseParam("-b, --benchmark <NUM>") catch unreachable,
-        clap.parseParam("-i, --show-info") catch unreachable,
-        clap.parseParam("-f, --file <FILE>") catch unreachable,
-        clap.parseParam("-n, --no-sort") catch unreachable,
-        clap.parseParam("-v, --version") catch unreachable,
+    const params = comptime clap.parseParamsComptime(
+        \\ -l, --lines <LINES>        Specify how many lines of results to show (default 10)
+        \\ -p, --prompt <PROMPT>      Input prompt (default '> ')
+        \\ -q, --query <QUERY>        Use QUERY as the initial search string
+        \\ -e, --show-matches <QUERY> Output the sorted matches of QUERY
+        \\ -t, --tty <TTY>            Specify file to use as TTY device (default /dev/tty)
+        \\ -s, --show-scores          Show the scores of each match
+        \\ -0, --read-null            Read input delimited by ASCII NUL characters
+        \\ -j, --workers <NUM>        Use NUM workers for searching. (default is # of CPUs)
+        \\ -b, --benchmark <NUM>      Run the match algorithm NUM times
+        \\ -i, --show-info            Show selection info line
+        \\ -f, --file <FILE>          Read choices from FILE insted of stdin
+        \\ -n, --no-sort              Do not sort matches
+        \\ -h, --help                 Display this help and exit
+        \\ -v, --version              Output version information and exit
+    );
+
+    const usage = struct {
+        fn usage() !void {
+            try stderr.writeAll("Usage: fzy [OPTION]...\n");
+            try clap.help(stderr, clap.Help, &params, .{
+                .description_on_new_line = false,
+                .spacing_between_parameters = 0,
+            });
+        }
+    }.usage;
+
+    const parsers = comptime .{
+        .LINES = clap.parsers.string,
+        .PROMPT = clap.parsers.string,
+        .QUERY = clap.parsers.string,
+        .TTY = clap.parsers.string,
+        .NUM = clap.parsers.int(u32, 10),
+        .FILE = clap.parsers.string,
     };
 
     var diag = clap.Diagnostic{};
-    var args = clap.parse(clap.Help, &params, .{ .diagnostic = &diag }) catch |err| {
-        diag.report(std.io.getStdErr().writer(), err) catch {};
+    var res = clap.parse(clap.Help, &params, parsers, .{
+        .diagnostic = &diag,
+    }) catch |err| {
+        diag.report(stderr, err) catch {};
         return err;
     };
-    defer args.deinit();
+    defer res.deinit();
 
-    if (args.flag("--help")) {
-        usage();
+    if (res.args.help) {
+        try usage();
         std.process.exit(0);
     }
 
-    if (args.option("--lines")) |l| {
+    if (res.args.lines) |l| {
         options.num_lines = blk: {
             if (std.fmt.parseUnsigned(usize, l, 10)) |lines| {
                 if (lines >= 3) {
@@ -81,50 +81,43 @@ pub fn new() !Options {
                 }
             } else |_| {}
             std.debug.print("Invalid format for --lines: {s}", .{l});
-            usage();
+            // usage();
             std.process.exit(1);
         };
     }
 
-    if (args.option("--prompt")) |p|
+    if (res.args.prompt) |p|
         options.prompt = p;
 
-    if (args.option("--query")) |q|
+    if (res.args.query) |q|
         options.init_search = q;
 
-    if (args.option("--show-matches")) |e|
+    if (res.args.@"show-matches") |e|
         options.filter = e;
 
-    if (args.option("--tty")) |t|
+    if (res.args.tty) |t|
         options.tty_filename = t;
 
-    if (args.flag("--show-scores"))
+    if (res.args.@"show-scores")
         options.show_scores = true;
 
-    if (args.flag("--read-null"))
+    if (res.args.@"read-null")
         options.input_delimiter = 0;
 
-    if (args.option("--workers")) |j|
-        options.workers = std.fmt.parseUnsigned(usize, j, 10) catch {
-            usage();
-            std.process.exit(1);
-        };
+    if (res.args.workers) |j|
+        options.workers = j;
 
-    if (args.flag("--show-info"))
+    if (res.args.@"show-info")
         options.show_info = true;
 
-    if (args.option("--benchmark")) |b| {
-        options.benchmark = std.fmt.parseUnsigned(u32, b, 10) catch {
-            usage();
-            std.process.exit(1);
-        };
-    }
+    if (res.args.benchmark) |b|
+        options.benchmark = b;
 
-    if (args.option("--file")) |f| {
+    if (res.args.file) |f| {
         options.input_file = f;
     }
 
-    if (args.flag("--no-sort"))
+    if (res.args.@"no-sort")
         options.sort = false;
 
     return options;
